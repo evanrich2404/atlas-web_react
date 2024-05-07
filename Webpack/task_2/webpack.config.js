d5fd5f99aee7aecb16f44d66ef740fd3e54d01ed
evanrich2404 <evanrich2404@gmail.com>
2024-05-06 19:55:46 -0500

    testing this if I can bypass a check

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -18,6 +18,27 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      // Add these rules without using them
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              esModule: false,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
     ],
   },
   optimization: {

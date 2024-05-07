889542fe2ce3d22bd15db85853519fbe6901d461
evanrich2404 <evanrich2404@gmail.com>
2024-05-07 16:40:52 -0500

    still stuck on the same spot and trying to figure out exactly what is wrong

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
-    assetModuleFilename: 'images/[name][ext]',
   },
   module: {
     rules: [
@@ -17,6 +16,26 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              esModule: false,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
   },

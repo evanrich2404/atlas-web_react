5a55bb006a38ed452878fd8b2ad8cff443ffc59b
evanrich2404 <evanrich2404@gmail.com>
2024-05-06 19:44:19 -0500

    more

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -19,6 +19,22 @@ module.exports = {
         generator: {
           filename: 'images/[name][ext]',
         },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
   },

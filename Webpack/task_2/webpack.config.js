1e5998eeb72defcd70a053dc3fa4e9a723332f1c
evanrich2404 <evanrich2404@gmail.com>
2024-05-06 19:28:46 -0500

    copied the last task and added some css stuff

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+};

b7fba0a08626af16e34b907e305cdd3a7929a4f5
evanrich2404 <evanrich2404@gmail.com>
2024-05-08 15:51:10 -0500

    pushing to check with the checker, having trouble checking the errors

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='App-header-content'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </div>
+      </header>
+      <body className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          {getFooterCopy(true)} - {getFullYear()}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

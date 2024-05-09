693af8b8ef0765be97587ea35eb55c0bfcefff88
evanrich2404 <evanrich2404@gmail.com>
2024-05-08 23:42:44 -0500

    task 2 and 3

--- react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -7,7 +7,7 @@ const Notifications = () => {
     <div className="Notifications">
       <p>Here is the list of notifications</p>
       <ul>
-        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New course available" />
         <NotificationItem type="urgent" value="New resume available" />
         <NotificationItem html={{ __html: '<strong>Urgent requirement</strong> - complete by EOD' }} />
       </ul>

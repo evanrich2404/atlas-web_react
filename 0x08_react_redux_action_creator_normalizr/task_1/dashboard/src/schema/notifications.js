3b5ce7fdb3484e4de7903fd9a93870fe3e01f1c3
evanrich2404 <evanrich2404@gmail.com>
2024-05-21 13:47:21 -0500

    I have normalized data, and tested for it

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,7 +1,27 @@
+import { normalize, schema } from 'normalizr';
 import * as notifData from '../../../notifications.json';
 
-export const getAllNotificationsByUser = (userId) => {
+// User entity
+const user = new schema.Entity('users');
+
+// Message entity with guid as the idAttribute
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Notification entity
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Normalize notifData
+const normalizedData = normalize(notifData.default, [notification]);
+
+// get all of the notifications based of the user's id
+const getAllNotificationsByUser = (userId) => {
   return notifData.default
   .filter(notification => notification.author.id === userId)
   .map(notification => notification.context);
 };
+
+// export ze normalized data
+export { normalizedData, getAllNotificationsByUser };

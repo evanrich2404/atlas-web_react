6e6aa56736eca8f5fc5febd5928c8d808264acf7
evanrich2404 <evanrich2404@gmail.com>
2024-05-28 17:56:27 -0500

    task 2 init

---
+++ react_redux_reducer_selector/task_2/dashboard/src/schema/notifications.js
@@ -0,0 +1,30 @@
+import { normalize, schema } from 'normalizr';
+import * as notifData from '../../../notifications.json';
+
+// Define a user entity
+const user = new schema.Entity('users');
+
+// Define a message entity with guid as the idAttribute
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Define a notification entity
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Normalize the data
+const normalizedData = normalize(notifData.default, [notification]);
+
+// Function to get all notifications by user using the normalized data
+export const getAllNotificationsByUser = (userId) => {
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+
+  return Object.values(notifications)
+    .filter(notification => notification.author === userId)
+    .map(notification => messages[notification.context]);
+};
+
+// Export the normalized data
+export { normalizedData };
